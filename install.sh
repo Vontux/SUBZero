db1b28b5efc2c2af601f65849c421c082dc2702d
Vontux <mbeaven001@gmail.com>
2020-03-12 19:45:07 -0700

    Added installation of uuidgen
    install uuidgen via installation of the uuid-runtime package

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade
 sudo apt install -y hostapd dnsmasq
+sudo apt install -y uuid-runtime
 sudo mkdir -p /var/www/SUBZero/ && sudo cp ./httpsServer.py /var/www/SUBZero && sudo cp wallpaper.jpg /var/www/SUBZero &&  openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes && sudo cp ./server.pem /var/www/SUBZero
  
 chmod +x ./subzero.sh && sudo cp ./subzero.sh /usr/local/bin
